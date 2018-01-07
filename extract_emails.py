92dcdb92d069d65f420a4c10a9c63ecc3586770f
Adonais0 <chuyao@umich.edu>
2018-01-06 23:47:41 -0500

    need to check regex again

--- extract_emails.py
+++ extract_emails.py
@@ -2,6 +2,8 @@ import sys
 import re
 
 print(sys.argv)
+infile = str(sys.argv[1])
+outfile = str(sys.argv[2])
 def test_patterns(text, patterns = []):
     print
     for pattern in patterns:
@@ -10,14 +12,18 @@ def test_patterns(text, patterns = []):
             match = p.search(text)
             s = match.start()
             e = match.end()
-            print(text[s:e])
+            data = text[s:e]
         except:
-            print(None)
-    return
+            data = "None"
+        print(data)
+    return data+'\n'
 
 if __name__ == '__main__':
-    test_patterns('abbaaabbbaaaaa',['ab'])
-    f = open('trial-pages.txt','r')
-    lines = f.readlines()
+    i = open(infile,'r')
+    lines = i.readlines()
+    ls = []
     for line in lines:
-        test_patterns(line,['([\w|\d]+\s?(@|at){1}([\s]?)[\w|\d]*([\s]?)(\.|dot){1}([\s]?)[\w]+)'])
+        ls.append(test_patterns(line,['([\w|\d]+\s?(@|at){1}([\s]?)[\w|\d]*([\s]?)(\.|dot){1}([\s]?)[\w]+)']))
+    o = open(outfile,'w')
+    for line in ls:
+        o.write(line)
